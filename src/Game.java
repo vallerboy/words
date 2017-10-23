0c8527ee747cd8cb1b219b31261a4f25fdcca833
Oskar <oskarbank@gmail.com>
2017-10-23 17:20:40 +0200

    Initial commit

---
+++ src/Game.java
@@ -0,0 +1,66 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+
+    private Scanner scanner;
+    private Random random;
+    private int round;
+    private String[] wordsArray;
+    private long timeStart;
+
+    public Game() {
+        scanner = new Scanner(System.in);
+        random = new Random();
+        wordsArray = UtilsWords.WORDS.split(" ");
+    }
+
+
+    public void start() {
+        round = 0;
+        printMenu();
+
+
+    }
+
+    private void printMenu() {
+        System.out.println("Witaj w magicznej grze");
+        System.out.println("Musisz odgadnac zapisane wyrazy");
+
+        timeStart = System.currentTimeMillis();
+        for(int i = 0; i < 12; i++){
+            nextWord();
+        }
+
+        System.out.println("Koniec gry!");
+        System.out.println("Twoj czas to: " + (System.currentTimeMillis() - timeStart)/1000  + " s");
+    }
+
+    private String getRandomWord(){
+        return wordsArray[random.nextInt(wordsArray.length-1)];
+    }
+
+    private String hashWord(String word){
+        for(int i = 0; i <= 1; i++) {
+            char randomChar = word.charAt(random.nextInt(word.length()));
+            if(randomChar != '*') {
+                word = word.replaceFirst(String.valueOf(randomChar), "*");
+            }
+        }
+        return word;
+    }
+
+    private void nextWord() {
+        String originalWord = getRandomWord();
+
+        System.out.println("Twój wyraz: " + hashWord(originalWord));
+        System.out.print("Podaj słowo: ");
+        String typedWord = scanner.nextLine();
+
+        if(!typedWord.equalsIgnoreCase(typedWord)){
+            timeStart -= 5 * 1000;
+            System.out.println("BLAD!");
+        }
+    }
+
+}
---
+++ src/UtilsWords.java
@@ -0,0 +1,3 @@
+public class UtilsWords {
+    public static final String WORDS = "ten kto przed siebie idzie niczego nie traci Nie uciekaj przed życiem bo Ci się to nie opłaci";
+}
